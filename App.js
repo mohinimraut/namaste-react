eb5be265475258db21dd0d393f98bbd88dfeb06f
mohinimraut <mohiniraut7585@gmail.com>
2024-01-07 23:27:27 +0530

    session3 completed

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
  import React from 'react';
 import ReactDOM from 'react-dom';
- const heading=React.createElement("div",{id:"parent"},
-[
-React.createElement("div",{id:"child"},[React.createElement("h1",{},"I am in heading-1 very crazy"),React.createElement("h2",{},"I am in heading-2")]),
-React.createElement("div",{id:"child2"},[React.createElement("h1",{},"C I am in heading-1"),React.createElement("h2",{},"C I am in heading-2")])
-]
+
+const elem=<span>React Element</span>;
+
+const title=(
+    <h1 className='head' tabIndex="5">
+     {elem}----   
+     Namaste React using JSX......
+    </h1>
+);
+
+const HeadingComponent=()=>(
+    <div id="container">
+    {title}
+    <h1 className='heading'>Namaste React Functional Component</h1>
+    </div>
 )
-const root=ReactDOM.createRoot(document.getElementById("root"))
-root.render(heading)
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent/>)
