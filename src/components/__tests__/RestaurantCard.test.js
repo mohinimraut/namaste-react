6b8269326235649aedb5cc80c721295aa03aae81
mohinimraut <mohiniraut7585@gmail.com>
2024-02-11 16:20:50 +0530

    testing higher order component

--- src/components/__tests__/RestaurantCard.test.js
+++ src/components/__tests__/RestaurantCard.test.js
@@ -1,11 +1,20 @@
 import { render, screen } from "@testing-library/react";
-import RestaurantCard from "../RestaurantCard";
+import RestaurantCard, { withPromotedLabel } from "../RestaurantCard";
 import MOCK_DATA from "../mocks/resCardMock.json";
 
 import "@testing-library/jest-dom";
 console.log("mockkkkkk", MOCK_DATA);
+
 test("RestaurantCard should load with props data", () => {
   render(<RestaurantCard resData={MOCK_DATA} />);
+  const name = screen.getByText("Burger King");
+  expect(name).toBeInTheDocument();
+});
+
+it("should render RestaurantCard component with Promoted Label RestaurantCard passed as arguement", () => {
+ 
+  const RestaurantCardPromoted = withPromotedLabel(RestaurantCard);
+  render(<RestaurantCardPromoted resData={MOCK_DATA} />);
   const name = screen.getByText("Burger King");
   expect(name).toBeInTheDocument();
 });
